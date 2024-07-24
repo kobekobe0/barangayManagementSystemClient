35e78b23489756c2d13389edecf1e7959faf30e4
Kobe Brian Santos <kobe0santos0@gmail.com>
2024-07-25 03:15:33 +0800

    added readOnly option on the inputfield

--- src/components/receipts/InputField.jsx
+++ src/components/receipts/InputField.jsx
@@ -1,16 +1,17 @@
 import React from "react"; // We only need React here
 
-const InputField = React.memo(({ label, value, onChange, width }) => {
+const InputField = React.memo(({ label, value, onChange, width, editable }) => {
   // Remove console.log for production (optional)
   console.log("InputField rendered", label, value, width);
 
   return (
     <input
-      className={`${width} border px-2 py-1 text-lg`}
+      className={`${width} border px-2 py-1 text-lg ${label == 'Amount' ? value <= 0 ? 'font-normal text-gray-600' : 'font-medium' : ''}`}
       type="text"
       placeholder={label}
       onChange={onChange}
       value={value || ''} // Set default value if undefined
+      readOnly={editable !== undefined ? !editable : false}
     />
   );
 }, (prevProps, nextProps) => prevProps.value === nextProps.value); // Compare values
